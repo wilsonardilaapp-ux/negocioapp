f2ea6d908e6dc4b02ebee1d0c8113a12a14ba7e5
Wilson Ardila <wilsonardilaapp@gmail.com>
2026-03-21 02:34:12 +0000

    aplica  esto intrucines:# Eliminar las páginas duplicadas fuera del rout

--- src/app/dashboard/blog/page.tsx
+++ src/app/dashboard/blog/page.tsx
@@ -1,4 +1,3 @@
-
 // This file is intentionally left empty to resolve a build conflict.
 // The correct page is located at /src/app/(dashboard)/dashboard/blog/page.tsx
 export default function Page() {
