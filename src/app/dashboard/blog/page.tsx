3961558c1150c1848345cc8de060d52306dc7960
Wilson Ardila <wilsonardilaapp@gmail.com>
2026-03-21 02:55:30 +0000

    Try fixing this error: `Server Error: Error: Cannot find module '/home/u

--- src/app/dashboard/blog/page.tsx
+++
@@ -1,5 +0,0 @@
-// This file is intentionally left empty to resolve a build conflict.
-// The correct page is located at /src/app/(dashboard)/dashboard/blog/page.tsx
-export default function Page() {
-  return null;
-}
