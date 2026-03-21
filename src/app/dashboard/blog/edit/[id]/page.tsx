7e589a53c0554244c247734108a1c278e596ac4c
Wilson Ardila <wilsonardilaapp@gmail.com>
2026-03-21 02:20:22 +0000

    npm run build
    > nextn@0.1.0 build
    > NODE_ENV=production next build

--- src/app/dashboard/blog/edit/[id]/page.tsx
+++ src/app/dashboard/blog/edit/[id]/page.tsx
@@ -1,6 +1,6 @@
 
 // This file is intentionally left empty to resolve a build conflict.
-// The correct page is located at /src/app/(dashboard)/dashboard/blog/create/page.tsx
+// The correct page is located at /src/app/(dashboard)/dashboard/blog/edit/[id]/page.tsx
 export default function Page() {
   return null;
 }
