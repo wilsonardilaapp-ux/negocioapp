4dc53210aaca493eb7d1aa7c909bd8b2b2fc15f7
Wilson Ardila <wilsonardilaapp@gmail.com>
2026-03-29 01:48:49 +0000

    Actúa como un desarrollador senior full stack especializado en SaaS con

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 
 export const firebaseConfig = {
   "apiKey": "AIzaSyBAgWCpQgSA6RM77VErau9dITvoerJ0VZk",
-  "authDomain": "negociod-v03-14457184.firebaseapp.com",
-  "projectId": "negociod-v03-14457184",
-  "storageBucket": "negociod-v03-14457184.appspot.com",
+  "authDomain": "negociod-v03-14457184-f8e95.firebaseapp.com",
+  "projectId": "negociod-v03-14457184-f8e95",
+  "storageBucket": "negociod-v03-14457184-f8e95.appspot.com",
   "messagingSenderId": "343827758456",
   "appId": "1:343827758456:web:f8833411e8a3615a18413a",
   "measurementId": "G-L5V88YV5L6"
