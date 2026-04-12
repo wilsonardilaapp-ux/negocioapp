c17e20bfce6578bb20d89d5cd6a0d209830df409
Wilson Ardila <wilsonardilaapp@gmail.com>
2026-04-12 23:50:26 +0000

    Actúa como un desarrollador senior full stack especializado en SaaS con

--- next.config.ts
+++ next.config.ts
@@ -40,7 +40,19 @@ const nextConfig: NextConfig = {
             {
                 protocol: 'https',
                 hostname: 'img.freepik.com',
-            }
+            },
+            {
+                protocol: 'https',
+                hostname: 'firebasestorage.googleapis.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'storage.googleapis.com',
+            },
         ],
     },
 };
